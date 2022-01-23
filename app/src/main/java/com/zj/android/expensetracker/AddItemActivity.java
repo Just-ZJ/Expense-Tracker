007422d9ddf37dad4c18b85348c7fa6bf73ea058
Zheng Ji Tan <dunlike2big@hotmail.com>
2022-01-23 16:58:08 -0500

    added chips to view and implemented some functionality of it

--- app/src/main/java/com/zj/android/expensetracker/AddItemActivity.java
+++ app/src/main/java/com/zj/android/expensetracker/AddItemActivity.java
@@ -1,14 +1,48 @@
 package com.zj.android.expensetracker;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    private TextView mDateTextView;
+    private ImageView mSelectDateImageView;
+    private ChipGroup mCategoriesChipGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        mDateTextView = findViewById(R.id.textView_date);
+        mSelectDateImageView = findViewById(R.id.imageView_select_date);
+        mSelectDateImageView.setOnClickListener(view -> {
+            // TODO: open the datepicker fragment
+        });
+        mCategoriesChipGroup = findViewById(R.id.chipGroup_expense_category);
+        populateChipGroup();
+    }
+
+    private void populateChipGroup(){
+//        TODO: change this to create from an array of values
+        mCategoriesChipGroup.addView(addChip("Grocery"));
+        mCategoriesChipGroup.addView(addChip("Fuel"));
+        mCategoriesChipGroup.addView(addChip("Dining"));
+        mCategoriesChipGroup.addView(addChip("Subscriptions"));
+        mCategoriesChipGroup.addView(addChip("Miscellaneous"));
+    }
+    private Chip addChip(String category) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        Chip newChip = (Chip) inflater.inflate(R.layout.action_add_chip, this.mCategoriesChipGroup, false);
+        newChip.setText(category);
+        return newChip;
     }
 }
