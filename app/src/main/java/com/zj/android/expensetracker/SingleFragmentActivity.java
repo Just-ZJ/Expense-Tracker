967ce38bfcbf7d7daa8a48e52d8166b808226a6c
Zheng Ji Tan <dunlike2big@hotmail.com>
2022-01-29 18:42:36 -0500

    added pageviewer to slide through pages

--- app/src/main/java/com/zj/android/expensetracker/SingleFragmentActivity.java
+++ app/src/main/java/com/zj/android/expensetracker/SingleFragmentActivity.java
@@ -1,24 +1,25 @@
 package com.zj.android.expensetracker;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.zj.android.expensetracker.add_item.AddItemActivity;
-import com.zj.android.expensetracker.dashboard.DashboardActivity;
-import com.zj.android.expensetracker.transaction.TransactionActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.zj.android.expensetracker.add_item.AddItemFragment;
+import com.zj.android.expensetracker.dashboard.DashboardFragment;
+import com.zj.android.expensetracker.transaction.TransactionFragment;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    FragmentPagerAdapter mPagerAdapter;
+    ViewPager mViewPager;
 
     protected abstract Fragment createFragment();
 
@@ -27,32 +28,47 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = createFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
 
+
+        mViewPager = findViewById(R.id.fragment_container);
+        mPagerAdapter = new CustomFragmentStateAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+
+        // on click events for bottom app bar
+        BottomNavigationItemView menu_dashboard = findViewById(R.id.bottom_appbar_dashboard);
+        menu_dashboard.setOnClickListener(view -> mViewPager.setCurrentItem(0, true));
+        FloatingActionButton menu_fab = findViewById(R.id.floating_action_button);
+        menu_fab.setOnClickListener(view -> mViewPager.setCurrentItem(1, true));
+        BottomNavigationItemView menu_transaction = findViewById(R.id.bottom_appbar_transaction);
+        menu_transaction.setOnClickListener(view -> mViewPager.setCurrentItem(2, true));
     }
 
-    public void startDashboard(MenuItem item){
-        Intent intent = new Intent(this, DashboardActivity.class);
-        startActivity(intent);
+    public static class CustomFragmentStateAdapter extends FragmentPagerAdapter {
+
+        public CustomFragmentStateAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new DashboardFragment();
+                case 1:
+                    return new AddItemFragment();
+                case 2:
+                    return new TransactionFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
     }
-
-    public void startAddItem(MenuItem item){
-        Intent intent = new Intent(this, AddItemActivity.class);
-        startActivity(intent);
-    }
-
-    public void startTransaction(MenuItem item){
-        Intent intent = new Intent(this, TransactionActivity.class);
-
-        startActivity(intent);
-    }
-
 }
