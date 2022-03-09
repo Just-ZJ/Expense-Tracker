750bf5bab3ff08295bebb36f40267671c1927518
Zheng Ji Tan <dunlike2big@hotmail.com>
2022-03-08 21:41:24 -0500

    added cursorwrapper to get tuples from database

---
+++ app/src/main/java/com/zj/android/expensetracker/DatabaseCursorWrapper.java
@@ -0,0 +1,58 @@
+package com.zj.android.expensetracker;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.zj.android.expensetracker.database.CategoryDbSchema.CategoryTable;
+import com.zj.android.expensetracker.database.ExpenseDbSchema.ExpenseTable;
+import com.zj.android.expensetracker.database.ExpenseToCategoryDbSchema.ExpenseToCategoryTable;
+import com.zj.android.expensetracker.models.Category;
+import com.zj.android.expensetracker.models.Expense;
+import com.zj.android.expensetracker.models.ExpenseToCategory;
+
+import java.util.UUID;
+
+public class DatabaseCursorWrapper extends CursorWrapper {
+
+    public DatabaseCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    /*************************** getting tuples from database **********************/
+    public Category getCategory() {
+        String uuidString = getString(getColumnIndex(CategoryTable.Cols.UUID));
+        String title = getString(getColumnIndex(CategoryTable.Cols.NAME));
+
+        Category category = new Category(UUID.fromString(uuidString));
+        category.setName(title);
+
+        return category;
+    }
+
+    public Expense getExpense() {
+        String uuidString = getString(getColumnIndex(ExpenseTable.Cols.UUID));
+        String date = getString(getColumnIndex(ExpenseTable.Cols.DATE));
+        String details = getString(getColumnIndex(ExpenseTable.Cols.DETAILS));
+        String amount = getString(getColumnIndex(ExpenseTable.Cols.AMOUNT));
+
+        Expense expense = new Expense(UUID.fromString(uuidString));
+        expense.setDate(date);
+        expense.setDetails(details);
+        expense.setAmount(Double.parseDouble(amount));
+
+        return expense;
+    }
+
+    public ExpenseToCategory getExpenseToCategory() {
+        String uuidString = getString(getColumnIndex(ExpenseToCategoryTable.Cols.UUID));
+        String expenseId = getString(getColumnIndex(ExpenseToCategoryTable.Cols.EXPENSE_ID));
+        String categoryId = getString(getColumnIndex(ExpenseToCategoryTable.Cols.CATEGORY_ID));
+
+        ExpenseToCategory expenseToCategory = new ExpenseToCategory(UUID.fromString(uuidString));
+        expenseToCategory.setExpenseId(UUID.fromString(expenseId));
+        expenseToCategory.setCategoryId(UUID.fromString(categoryId));
+
+        return expenseToCategory;
+    }
+
+}
